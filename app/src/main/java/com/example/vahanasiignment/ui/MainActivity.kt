45583c3639d0c70f10aa8e77383314ca752ea6f0
Navin Sah <navinkrsah17@gmail.com>
2023-09-25 18:00:12 +0530

    toolbar added foreground permission managed

--- app/src/main/java/com/example/vahanasiignment/ui/MainActivity.kt
+++ app/src/main/java/com/example/vahanasiignment/ui/MainActivity.kt
@@ -1,33 +1,46 @@
 package com.example.vahanasiignment.ui
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.vahanasiignment.refresher.DataRefreshService
 import com.example.vahanasiignment.R
+import com.example.vahanasiignment.databinding.ActivityMainBinding
 import com.example.vahanasiignment.repository.UniversityRepository
 import com.example.vahanasiignment.ui.fragments.UniversityViewModelFactoryProvider
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: UniversityViewModel
+    lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        onClickRequestPermission(binding.root)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+        /*if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
             // Request notification permission here
             showNotificationPermissionDialog()
-        }
+        }*/
 
 
         val viewModelProviderFactory = UniversityViewModelFactoryProvider(application,universityRepository = UniversityRepository())
@@ -44,6 +57,45 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Log.d("Permission: ", "Granted")
+        } else {
+            Log.d("Permission: ", "Denied")
+        }
+    }
+
+    private fun onClickRequestPermission(view: View) {
+        when {
+            ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("Permission: ", "Granted")
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) -> {
+                Snackbar.make(
+                    view,
+                    "Allow Notification",
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction("OK") {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.POST_NOTIFICATIONS
+                    )
+                }.setActionTextColor(resources.getColor(android.R.color.holo_red_light)).show()
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+            }
+        }
     }
 
     private fun showNotificationPermissionDialog() {
